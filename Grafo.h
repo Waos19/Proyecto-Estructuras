0d3cdad8092ee0d8cb3636c430dcdcc529636bbc
David <davidstevengarzon3@gmail.com>
2025-05-17 13:37:25 -0500

    Proyecto seteado

---
+++ Grafo.h
@@ -0,0 +1,16 @@
+#ifndef GRAFO_H
+#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+class Grafo
+{
+    private:
+        int V; // Número de vértices
+        list<int> *adj; // Lista de adyacencia
+};
+
+
+#endif
